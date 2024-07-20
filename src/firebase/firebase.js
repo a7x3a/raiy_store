ba3669a1a3aa2b1555391de8c47abd707b79f069
Ahmad Omar <116672329+a7x3a@users.noreply.github.com>
2024-07-20 23:35:23 +0300

    Updated Enviroment Varibles tr2 firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 const {API_KEY,APP_ID,AUTH_DOMAIN,MEASUREMENT_ID,MESSAGING_SENDERID,PROJECT_ID,STORAGE_BUCKET} = import.meta.env;
 const firebaseConfig = {
-  apiKey: "AIzaSyBo_ODg2WV0Yh_0SzqAEx4k2mwN0BxsptI",
-  authDomain: "raiystore.firebaseapp.com",
-  projectId: "raiystore",
-  storageBucket: "raiystore.appspot.com",
-  messagingSenderId: "618400888880",
-  appId: "1:618400888880:web:6ba16b9c8340885cce15f5",
-  measurementId: "G-RF4GG5699G",
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDERID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
 };
 
 // Initialize Firebase
