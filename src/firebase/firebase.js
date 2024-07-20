cf9775dc51db0c7cc3deca50aba22ece1c34462c
Ahmad Omar <116672329+a7x3a@users.noreply.github.com>
2024-07-21 00:18:01 +0300

    Updated firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,15 +2,26 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
+
+ console.log(import.meta.env.API_KEY);
+  console.log(import.meta.env.AUTH_DOMAIN);
+  console.log(import.meta.env.PROJECT_ID);
+  console.log(import.meta.env.STORAGE_BUCKET);
+  console.log(import.meta.env.MESSAGING_SENDER_ID);
+  console.log(import.meta.env.APP_ID);
+  console.log(import.meta.env.MEASUREMENT_ID);
+
 const firebaseConfig = {
-  apiKey: import.meta.env.API_KEY,
-  authDomain: import.meta.env.AUTH_DOMAIN,
-  projectId: import.meta.env.PROJECT_ID,
-  storageBucket: import.meta.env.STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
-  appId: import.meta.env.APP_ID,
-  measurementId: import.meta.env.MEASUREMENT_ID,
+  apiKey: "AIzaSyBo_ODg2WV0Yh_0SzqAEx4k2mwN0BxsptI",
+  authDomain: "raiystore.firebaseapp.com",
+  databaseURL: "https://raiystore-default-rtdb.firebaseio.com",
+  projectId: "raiystore",
+  storageBucket: "raiystore.appspot.com",
+  messagingSenderId: "618400888880",
+  appId: "1:618400888880:web:6ba16b9c8340885cce15f5",
+  measurementId: "G-RF4GG5699G"
 };
+
 
 
 // Initialize Firebase
